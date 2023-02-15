fc4d01235366342a3d9aff60965e878264d2ece7
shuib <shuaib_97@hotmail.co.uk>
2023-02-15 19:46:06 +0000

    refactored original code

--- index.js
+++ index.js
@@ -1,111 +1,134 @@
-const teamArray = [];
+let teamArray = [];
 
 const inquirer = require("inquirer");
 const fs = require("fs");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const { timingSafeEqual } = require("crypto");
 const generateHtml = require("./src/generateHtml");
-
-const managerQuestions = ["name", "id", "email", "officeNumber"];
-
-const inquirerManagerQuestions = managerQuestions.map((it) => {
-  return {
-    type: "input",
-    name: it,
-    message: it,
-  };
-});
-
-const engineerQuestions = ["name", "id", "email", "github"];
-
-const inquirerEngineerQuestions = engineerQuestions.map((it) => {
-  return {
-    type: "input",
-    name: it,
-    message: it,
-  };
-});
-
-const internQuestions = ["name", "id", "email", "school"];
-
-const inquirerInternQuestions = internQuestions.map((it) => {
-  return {
-    type: "input",
-    name: it,
-    message: it,
-  };
-});
 
 // askQuestions() just prompts to user with inquirerQuestions, using questions array
 
-const question = [
-  {
-    type: "list",
-    name: "choices",
-    message: "What would you like to do next?",
-    choices: ["Add engineer", "Add Intern"],
-  },
-];
 async function menu() {
-  let answers = await inquirer.prompt(question);
+  let answers = await inquirer.prompt([
+    {
+      type: "list",
+      name: "choices",
+      message: "What would you like to do next?",
+      choices: ["Add engineer", "Add Intern", "Done"],
+    },
+  ]);
   if (answers.choices === "Add engineer") {
     askEngineerQuestions();
   }
-
   if (answers.choices === "Add Intern") {
     askInternQuestions();
   }
-
+  if(answers.choices === "Done") {
+    writeFile(generateHtml(teamArray))
+  }
 }
 
-async function askManagerQuestions() {
-  await inquirer.prompt(inquirerManagerQuestions)
-  .then(managerData => {
-    const { name, id, email, officeNumber } = managerData;
-    const manager = new Manager(name, id, email, officeNumber)
-    teamArray.push(manager);
-    menu()
-  })
+const askManagerQuestions = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Email?",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "Office Number?",
+      },
+    ])
+    .then((managerData) => {
+      const { name, id, email, officeNumber } = managerData;
+      const manager = new Manager(name, id, email, officeNumber);
+      teamArray.push(manager);
+      menu();
+    });
+};
+
+const askEngineerQuestions = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Name?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Email?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "ID?",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "GitHub?",
+      },
+    ])
+    .then((engineerData) => {
+      const { name, id, email, github } = engineerData;
+      const engineer = new Engineer(name, id, email, github);
+      teamArray.push(engineer);
+      menu()
+    });
 }
 
-async function askEngineerQuestions() {
-  await inquirer.prompt(inquirerEngineerQuestions)
-  .then(engineerData => {
-    const { name, id, email, github } = engineerData
-    const engineer = new Engineer(name, id, email, github)
-    teamArray.push(engineer)
-  console.log(teamArray)
-})
-
+const askInternQuestions = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Name?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Email?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "ID?",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "School?",
+      },
+    ])
+    .then((internData) => {
+      const { name, id, email, school } = internData;
+      const intern = new Intern(name, id, email, school);
+      teamArray.push(intern);
+      menu()
+    });
 }
 
-async function askInternQuestions() {
-  await inquirer.prompt(inquirerInternQuestions)
-  .then(internData => {
-    const { name, id, email, school } = internData;
-    const intern = new Intern(name, id, email, school)
-    teamArray.push(intern);
-    console.log(teamArray)
-  })
-}
-
-const writeFile = data => {
-  fs.writeFile("./dist/index.html", data, err => {
-    console.log(data)
-    err ? console.log(err) : console.log("HTML created!")
-  })
-}
-
+const writeFile = (data) => {
+  fs.writeFile("./dist/index.html", data, (err) => {
+    err ? console.log(err) : console.log("HTML created!");
+  });
+};
 
 askManagerQuestions()
- .then(teamArray => {
-   return generateHtml(teamArray)
- })
- .then(data => {
-   return writeFile(data)
- })
- .catch(err => {
-   console.log(err)
- }) 
--- src/generateHtml.js
+++ src/generateHtml.js
@@ -1,58 +1,58 @@
-const managersCard = function(manager) {
-    return `<div class="card">
+const managerCard = function (manager) {
+  return `<div class="card">
     <h1>Manager</h1>
     <h3>Name: ${manager.name}</h3>
     <h3>Email: ${manager.email}</h3>
     <h3>Office Number: ${manager.officeNumber}</h3>
-    </div>`
-}
+    </div>`;
+};
 
-const internCard = function(intern) {
+const internCard = function (intern) {
     return `<div class="card">
     <h1>Intern</h1>
     <h3>Name: ${intern.name}</h3>
     <h3>Email: ${intern.email}</h3>
     <h3>Office Number: ${intern.school}</h3>
-    </div>`
-}
+    </div>`;
+};
 
-const engineerCard = function(engineer) {
+const engineerCard = function (engineer) {
     return `<div class="card">
     <h1>Engineer</h1>
     <h3>Name: ${engineer.name}</h3>
     <h3>Email: ${engineer.email}</h3>
-    <h3>Office Number: ${manager.github}</h3>
-    </div>`
-}
+    <h3>Office Number: ${engineer.github}</h3>
+    </div>`;
+};
 
-const generateHtml = function(data) {
-    let page = []
-    for(let i = 0; i < data.length; i++) {
-        const employee = data[i]
-        const role = employee.getRole()
+const generateHtml = function (data) {
+  let page = [];
+  for (let i = 0; i < data.length; i++) {
+    const employee = data[i];
+    const role = employee.getRole();
 
-        if(role === 'Manager') {
-            const managerCard = managersCard(employee)
-            page.push(managerCard)
-        }
-
-        if(role === 'Intern') {
-            const internCard = managersCard(employee)
-            page.push(internCard)
-        }
-
-        if(role === 'Engineer') {
-            const engineerCard = managersCard(employee)
-            page.push(engineerCard)
-        }
+    if (role === "Manager") {
+      const managersCard = managerCard(data[i]);
+      page.push(managersCard);
     }
-    const cards = page.join('')
-    const generateTeam = main(employeeCards)
-    return generateTeam
-}
 
-const main = function(employeeCards) {
-    return `<!DOCTYPE html>
+    if (role === "Intern") {
+      const internsCard = internCard(data[i]);
+      page.push(internsCard);
+    }
+
+    if (role === "Engineer") {
+      const engineersCard = engineerCard(data[i]);
+      page.push(engineersCard);
+    }
+  }
+  const cards = page.join("");
+  const generateTeam = main(cards);
+  return generateTeam;
+};
+
+const main = function (employeeCards) {
+  return `<!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
@@ -63,7 +63,7 @@ const main = function(employeeCards) {
     <body>
         ${employeeCards}
     </body>
-    </html>`
-}
+    </html>`;
+};
 
-module.exports = generateHtml
+module.exports = generateHtml;
